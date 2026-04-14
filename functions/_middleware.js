e602b763a129aa9b015a9f618490e3dae4ca26d7
Jo Hajin <hajing09@gmail.com>
2026-04-14 14:38:33 +0900

    Delete functions directory

--- functions/_middleware.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  console.log("Request:", {
-    url: context.request.url,
-    userAgent: context.request.headers.get("user-agent"),
-    ip: context.request.headers.get("cf-connecting-ip"),
-  });
-  return context.next();
-}
